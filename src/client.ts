cc09eb90f829e88fa5748157b193106bb78a6944
milosz <milosz.miotk@studio-creativa.pl>
2026-08-03 17:57:45 +0200

    update queries with new types and updated fetch

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,9 @@
-import type { ApiClient, RequestOptions } from "@dockcodes/fetcher";
+import type { ApiClient, RequestOptions } from '@dockcodes/fetcher';
 import type {
   GenerateBody,
   GenerateParams,
   GenerateResponse,
   GetImageParams,
-  GetImageResponse,
   GetImageSetImagesParams,
   GetImageSetImagesResponse,
   GetImageSetParams,
@@ -14,54 +13,32 @@ import type {
   LoginResponse,
   UpdateImageSetVisbilityBody,
   UpdateImageSetVisbilityResponse
-} from "./types";
+} from './types';
 
 function altGeneratorApi(client: ApiClient) {
-  return {
-    login(body: LoginBody, options?: RequestOptions) {
-      return client.POST<LoginResponse>("/token", body, options);
-    },
-    generate(body: GenerateBody & GenerateParams, options?: RequestOptions) {
-      return client.POST<GenerateResponse>("/generate", body, options);
-    },
-    getImage(params: GetImageParams, options?: RequestOptions) {
-      return client.GET<GetImageResponse>(
-        "/image/:image_uuid'",
-        params,
-        options,
-      );
-    },
-    getImageSet(params: GetImageSetParams, options?: RequestOptions) {
-      return client.GET<GetImageSetResponse>(
-        "/image-set/:image_set_uuid",
-        params,
-        options,
-      );
-    },
-    getImageSetImages(
-      params: GetImageSetImagesParams,
-      options?: RequestOptions,
-    ) {
-      return client.GET<GetImageSetImagesResponse>(
-        "/image-set/:id/images",
-        params,
-        options,
-      );
-    },
-    updateImageSetVisiblity(
-      body: UpdateImageSetVisbilityBody,
-      options?: RequestOptions,
-    ) {
-      return client.PATCH<UpdateImageSetVisbilityResponse>(
-        "/image-set/{id}/visibility",
-        body,
-        options,
-      );
-    },
-    getUser(options?: RequestOptions) {
-      return client.GET<GetUserResponse>("/users", undefined, options);
-    },
-  };
+    return {
+        login(body: LoginBody, options?: RequestOptions) {
+            return client.POST<LoginResponse>('/token', body, options);
+        },
+        generate(body: GenerateBody & GenerateParams, options?: RequestOptions) {
+            return client.POST<GenerateResponse>('/generate', body, options);
+        },
+        getImagePath(params: GetImageParams) {
+            return client.url('/image/:image_uuid', params);
+        },
+        getImageSet(params: GetImageSetParams, options?: RequestOptions) {
+            return client.GET<GetImageSetResponse>('/image-set/:image_set_uuid', params, options);
+        },
+        getImageSetImages(params: GetImageSetImagesParams, options?: RequestOptions) {
+            return client.GET<GetImageSetImagesResponse>('/image-set/:image_set_uuid/images', params, options);
+        },
+        updateImageSetVisiblity(body: UpdateImageSetVisbilityBody, options?: RequestOptions) {
+            return client.PATCH<UpdateImageSetVisbilityResponse>('/image-set/{id}/visibility', body, options);
+        },
+        getUser(options?: RequestOptions) {
+            return client.GET<GetUserResponse>('/user', undefined, options);
+        },
+    };
 }
 
 export default altGeneratorApi;
--- src/types.ts
+++ src/types.ts
@@ -1,59 +1,108 @@
-import type { languages, tones, variants } from "./consts";
+import type { languages, tones, variants } from './consts';
 
 type GeneratorTone = (typeof tones)[number];
 type GeneratorLanguage = (typeof languages)[number];
 type GeneratorVariants = (typeof variants)[number];
 
 type LoginBody = {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
 };
-type LoginResponse = string;
+type LoginResponse = {
+    access_token: string;
+    token_type: 'bearer';
+};
 
 type GenerateBody = {
-  file: string;
+    file: string;
 };
 type GenerateParams = {
-  context: string;
-  lang: GeneratorLanguage;
-  recaptcha_token?: string;
-  set_uuid?: string;
-  tone: GeneratorTone;
-  variants: GeneratorVariants;
+    context: string;
+    lang: GeneratorLanguage;
+    recaptcha_token?: string;
+    set_uuid?: string;
+    tone: GeneratorTone;
+    variants: GeneratorVariants;
 };
-type GenerateResponse = string;
+type GenerateResponse = {
+    data: {
+        set_uuid: string;
+        image: {
+            uuid: string;
+            filename: string;
+            alts: string[];
+        };
+    };
+};
 
-type GetImageParams = { image_uuid: string };
-type GetImageResponse = string;
+type GetImageParams = {
+    image_uuid: string;
+};
+type GetImageResponse = File;
 
-type GetImageSetParams = { image_set_uuid: string };
-type GetImageSetResponse = string;
+type GetImageSetParams = {
+    image_set_uuid: string;
+};
+type GetImageSetResponse = {
+    context: string;
+    created_at: string;
+    images_count: number;
+    is_owner: boolean;
+    language: 'en';
+    private: boolean;
+    tone: 'neutral';
+    uuid: string;
+    variants: number;
+};
 
 type GetImageSetImagesParams = {
-  image_set_uuid: string;
-  page: number;
-  per_page: number;
+    image_set_uuid: string;
+    page: number;
+    per_page: number;
 };
-type GetImageSetImagesResponse = string;
+type GetImageSetImagesResponse = {
+    data: {
+        uuid: string;
+        alts: { id: number; alt: string; locale: 'en'; created_at: string }[];
+        created_at: string;
+    }[];
+    paginate: {
+        page: number;
+        per_page: number;
+        total_items: number;
+    };
+};
 
-type UpdateImageSetVisbilityBody = { image_set_uuid: string; private: boolean };
-type UpdateImageSetVisbilityResponse = string;
+type UpdateImageSetVisbilityBody = {
+    image_set_uuid: string;
+    private: boolean;
+};
+type UpdateImageSetVisbilityResponse = {
+    success: boolean;
+    message: string;
+    data: {
+        uuid: string;
+        private: boolean;
+    };
+};
 
-type GetUserResponse = { requests: { limit: number; used: number } };
+type GetUserResponse = {
+    requests: { limit: number; used: number };
+};
 
 export type {
-  LoginBody,
-  LoginResponse,
-  GenerateBody,
-  GenerateResponse,
-  GenerateParams,
-  GetImageParams,
-  GetImageResponse,
-  GetImageSetImagesParams,
-  GetImageSetImagesResponse,
-  GetImageSetParams,
-  GetImageSetResponse,
-  UpdateImageSetVisbilityBody,
-  UpdateImageSetVisbilityResponse,
-  GetUserResponse,
+    LoginBody,
+    LoginResponse,
+    GenerateBody,
+    GenerateResponse,
+    GenerateParams,
+    GetImageParams,
+    GetImageResponse,
+    GetImageSetImagesParams,
+    GetImageSetImagesResponse,
+    GetImageSetParams,
+    GetImageSetResponse,
+    UpdateImageSetVisbilityBody,
+    UpdateImageSetVisbilityResponse,
+    GetUserResponse,
 };
